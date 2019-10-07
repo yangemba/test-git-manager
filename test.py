8658195273da91df45317b3890d484b17ea044ec
yan.hemba <yan.hemba@toweya.net>
2019-10-07 03:44:24 +0300

    add test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import argparse
+import logging
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-x", "--xarg", help="")
+    args = parser.parse_args()
+
+    x = int(args.xarg)
+
+    logging.warning(f' X^2 = {x ** 2}')
